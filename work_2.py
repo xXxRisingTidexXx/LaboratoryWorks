c532b0124c950295d3c6f2eb67cdf148a832f302
xXxRisingTidexXx <danylopetrik@gmail.com>
2018-09-12 21:16:04 +0300

    Integers were finished

---
+++ work_2.py
@@ -0,0 +1,78 @@
+"""
+Лабораторна робота №2
+ІПЗ - 12, Петраківський Данило
+"""
+
+
+def divide_integers():
+    """
+    #1
+    """
+    name = input('Ваше ім\'я: ')
+    x = int(input('Введіть X: '))
+    y = int(input('Введіть Y: '))
+    print('{} поділив {} на {}, отримавши {} цілих і {} в залишку, або {:f}'.format(name, x, y, x // y, x % y, x / y))
+
+
+def sum_digits():
+    """
+    #2
+    """
+    numb = input('Введіть число: ')
+    print('Сума цифр числа {0} складає {1}. 1 <= {1} <= 27'.format(numb, int(numb[0]) + int(numb[1]) + int(numb[2])))
+
+
+def calculate_last_digit():
+    """
+    #3
+    """
+    numb = int(input('Випадкове число: '))
+    print('Остання цифра: {}'.format(numb % 10))
+
+
+def convert_bits():
+    """
+    #4
+    """
+    total_bits = int(input('Випадкове число біт: '))
+    total_bytes = total_bits // 8
+    b = total_bytes % 1024
+    total_bytes //= 1024
+    kb = total_bytes % 1024
+    total_bytes //= 1024
+    mb = total_bytes % 1024
+    print('{} Мбайт, {} Кбайт, {} байт, {} біт'.format(mb, kb, b, total_bits % 8))
+
+
+def convert_seconds():
+    """
+    #5
+    """
+    seconds = int(input('Випадкове число секунд: '))
+    print('{} хвилина {} секунди '.format(seconds // 60, seconds % 60))
+
+
+def determinate_time():
+    """
+    #6
+    """
+    minutes = int(input('Випадкове число хвилин: '))
+    minutes %= 1440
+    print('{}:{}'.format(expand(minutes // 60), expand(minutes % 60)))
+
+
+def expand(n):
+    return str(n) if n >= 10 else '0{}'.format(n)
+
+
+def calculate_difference():
+    """
+    #7
+    """
+    h1 = int(input())
+    m1 = int(input())
+    s1 = int(input())
+    h2 = int(input())
+    m2 = int(input())
+    s2 = int(input())
+    print('{} сек.'.format((h2 - h1) * 3600 + (m2 - m1) * 60 + s2 - s1))

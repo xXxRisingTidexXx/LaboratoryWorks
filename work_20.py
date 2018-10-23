e3d60a520374aa379e9ea85cb68d69181241f78f
xXxRisingTidexXx <danylopetrik@gmail.com>
2018-10-23 12:11:20 +0300

    Oh these binary files! work_20 is completed

---
+++ work_20.py
@@ -0,0 +1,100 @@
+"""
+Лабораторна робота №20
+ІПЗ - 12, Петраківський Данило
+"""
+from struct import pack, unpack
+
+buffer = 4
+
+
+def create_integer_file():
+    """
+    #1
+    """
+    filename = input('Введіть ім\'я файлу: ')
+    n = int(input('Введіть n: '))
+    with open(filename, 'wb') as file:
+        for i in range(1, n + 1):
+            file.write(pack('i', i * 2))
+
+
+def create_arithmetic_progression_file():
+    """
+    #2
+    """
+    filename = input('Введіть ім\'я файлу: ')
+    a = float(input('Введіть a: '))
+    d = float(input('Введіть d: '))
+    with open(filename, 'wb') as file:
+        for i in range(10):
+            file.write(pack('f', a + d * i))
+
+
+def print_file_element():
+    """
+    #3
+    """
+    filename = input('Введіть ім\'я файлу: ')
+    k = int(input('Введіть k: ')) - 1
+    with open(filename, 'rb') as file:
+        file.seek(k * buffer)
+        print(unpack('I', file.read(buffer))[0])
+
+
+def create_absent_file():
+    """
+    #4
+    """
+    filename_1 = input('Введіть ім\'я 1 файлу: ')
+    filename_2 = input('Введіть ім\'я 2 файлу: ')
+    with open(filename_1, 'rb') as file_1, open(filename_2, 'wb') as file_2:
+        byte_list = file_1.read()
+        length = len(byte_list)
+        file_2.write(byte_list[slice(0, buffer)])
+        file_2.write(byte_list[slice(length - buffer, length)])
+
+
+def create_equal_file():
+    """
+    #5
+    """
+    filename_1 = input('Введіть ім\'я 1 файлу: ')
+    filename_2 = input('Введіть ім\'я 2 файлу: ')
+    with open(filename_1, 'rb') as file_1, open(filename_2, 'wb') as file_2:
+        byte_list = file_1.read()
+        for i in range(len(byte_list), 0, -buffer):
+            file_2.write(byte_list[slice(i - buffer, i)])
+
+
+def create_two_partial_files():
+    """
+    #6
+    """
+    filename_1 = input('Введіть ім\'я 1 файлу: ')
+    filename_2 = input('Введіть ім\'я 2 файлу: ')
+    filename_3 = input('Введіть ім\'я 3 файлу: ')
+    with open(filename_1, 'rb') as file_1, open(filename_2, 'wb') as file_2, open(filename_3, 'wb') as file_3:
+        byte_list = file_1.read()
+        for i in range(0, len(byte_list), buffer):
+            if i % 2 == 1:
+                file_2.write(byte_list[slice(i, i + buffer)])
+            else:
+                file_3.write(byte_list[slice(i, i + buffer)])
+
+
+def parse_file_elements():
+    """
+    #7
+    """
+    filename = input('Введіть ім\'я файлу: ')
+    with open(filename, 'rb') as file:
+        s = 0.0
+        m = 1.0
+        byte_list = file.read()
+        length = len(byte_list)
+        for i in range(0, length, buffer):
+            num = unpack('f', byte_list[slice(i, i + buffer)])[0]
+            s += num
+            m *= num
+        print('average arithmetic: %f  average geometric: %f' %
+              (s / length * buffer, m ** (1 / length * buffer)))
---
+++ work_21.py
@@ -0,0 +1,106 @@
+"""
+Лабораторна робота №21
+ІПЗ - 12, Петраківський Данило
+"""
+from struct import unpack, pack
+
+buffer = 4
+
+
+def write_squares():
+    """
+    #1
+    """
+    filename = input('Введіть ім\'я файлу: ')
+    with open(filename, 'r+b') as file:
+        byte_list = file.read()
+        file.seek(0)
+        for i in range(0, len(byte_list), buffer):
+            num = unpack('f', byte_list[slice(i, i + buffer)])[0]
+            file.write(pack('f', num * num))
+
+
+def rearrange_elements():
+    """
+    #2
+    """
+    filename = input('Введіть ім\'я файлу: ')
+    with open(filename, 'r+b') as file:
+        byte_list = file.read()
+        file.seek(0)
+        i = 0
+        j = len(byte_list) // buffer - 1
+        while j - i > 1:
+            file.write(byte_list[slice(i * buffer, (i + 1) * buffer)])
+            file.write(byte_list[slice(j * buffer, (j + 1) * buffer)])
+            i += 1
+            j -= 1
+        file.write(byte_list[slice(i * buffer, (i + 1) * buffer)])
+        if j > i:
+            file.write(byte_list[slice(j * buffer, (j + 1) * buffer)])
+
+
+def copy():
+    """
+    #3
+    """
+    filename_1 = input('Введіть ім\'я 1 файлу: ')
+    filename_2 = input('Введіть ім\'я 2 файлу: ')
+    with open(filename_1, 'rb') as file_1, open(filename_2, 'wb') as file_2:
+        file_2.write(file_1.read())
+
+
+def swap_contents():
+    """
+    #4
+    """
+    filename_1 = input('Введіть ім\'я 1 файлу: ')
+    filename_2 = input('Введіть ім\'я 2 файлу: ')
+    with open(filename_1, 'r+b') as file_1, open(filename_2, 'r+b') as file_2:
+        bytes_1 = file_1.read()
+        bytes_2 = file_2.read()
+        file_1.seek(0)
+        file_1.truncate()
+        file_1.write(bytes_2)
+        file_2.seek(0)
+        file_2.truncate()
+        file_2.write(bytes_1)
+
+
+def collect_files():
+    """
+    #5
+    """
+    main_filename = input('Введіть ім\'я головного файлу: ')
+    n = int(input('Введіть n: '))
+    filenames = [input('Введіь ім\'я %d файлу: ' % i) for i in range(n)]
+    with open(main_filename, 'wb') as main_file:
+        for filename in filenames:
+            with open(filename, 'rb') as file:
+                main_file.write(file.read())
+
+
+def append_data():
+    """
+    #6
+    """
+
+
+def delete_data():
+    """
+    #7
+    """
+
+
+def double_contents():
+    """
+    #8
+    """
+
+
+append_data()
+
+# with open('res/main', 'rb') as file:
+#     byte_list = file.read()
+#     for i in range(0, len(byte_list), buffer):
+#         print(unpack('i', byte_list[slice(i, i + buffer)])[0])

cf7ef5d2e2d9a7c41185749afd636960a1b9ebfc
xXxRisingTidexXx <danylopetrik@gmail.com>
2018-11-23 08:16:09 +0200

    Ball is moving; paddle is interactive a little bit as well

--- project_2.py
+++ project_2.py
@@ -2,58 +2,105 @@
 Проект №2
 ІПЗ - 12, Петраківський Данило
 """
+from random import choice
 from time import sleep
 from tkinter import *
 from yaml import load
 
-
-STRINGS = 'res/strings.yaml'
+PROPERTIES = 'res/properties.yaml'
+TK = 'tk'
+TITLE = 'title'
+RESIZABLE = 'resizesable'
+BALL = 'ball'
+PADDLE = 'paddle'
+GAME = 'game'
+DELAY = 'delay'
 
 
 class Game:
     def __init__(self):
-        self.tk = prepare_tk()
-        self.canvas = Canvas(self.tk, width=500, height=400, bd=0, highlightthickness=0)
-        self.canvas.pack()
+        self.data = self.__prepare_data()
+        self.tk = self.__prepare_tk()
+        canvas = self.__prepare_canvas()
+        self.ball = Ball(canvas, self.data['ball'])
+        self.paddle = Paddle(canvas, self.data['paddle'])
+        self.delay = self.data['game']['delay']
 
+    @staticmethod
+    def __prepare_data():
+        with open(PROPERTIES) as stream:
+            return load(stream)
 
-def prepare_tk():
-    tk = Tk()
-    with open(STRINGS) as stream:
-        tk.title(load(stream)['tk']['title'])
-    tk.resizable(0, 0)
-    tk.wm_attributes('-topmost', 1)
-    return tk
+    def __prepare_tk(self):
+        data = self.data['tk']
+        tk = Tk()
+        tk.title(data['title'])
+        tk.geometry(self.__prepare_geometry(tk.winfo_screenwidth(), tk.winfo_screenheight()))
+        tk.resizable(data['resizable'][0], data['resizable'][1])
+        tk.wm_attributes(data['wm_attributes'][0], data['wm_attributes'][1])
+        return tk
+
+    def __prepare_geometry(self, screen_width, screen_height):
+        w = self.data['canvas']['width']
+        h = self.data['canvas']['height']
+        return '{}x{}+{}+{}'.format(w, h, (screen_width - w) // 2, (screen_height - h) // 2)
+
+    def __prepare_canvas(self):
+        data = self.data['canvas']
+        canvas = Canvas(self.tk, width=data['width'], height=data['height'],
+                        bd=data['bd'], highlightthickness=data['highlightthickness'])
+        canvas.pack()
+        return canvas
+
+    def start(self):
+        while True:
+            self.ball.draw()
+            self.paddle.draw()
+            self.tk.update_idletasks()
+            self.tk.update()
+            sleep(self.delay)
 
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, data):
         self.canvas = canvas
-        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
-        self.canvas.move(self.id, 245, 100)
+        self.id = self.canvas.create_oval(data['x1'], data['y1'], data['x2'], data['y2'], fill=data['color'])
+        self.canvas.move(self.id, data['x0'], data['y0'])
+        self.dx = choice(data['dx'])
+        self.dy = choice(data['dy'])
 
     def draw(self):
-        pass
+        coords = self.canvas.coords(self.id)
+        self.dx = 1 if coords[0] <= 0 else -1 if coords[2] >= self.canvas.winfo_width() else self.dx
+        self.dy = 1 if coords[1] <= 0 else -1 if coords[3] >= self.canvas.winfo_height() else self.dy
+        self.canvas.move(self.id, self.dx, self.dy)
 
 
 class Paddle:
-    pass
+    def __init__(self, canvas, data):
+        self.canvas = canvas
+        self.id = self.canvas.create_rectangle(data['x1'], data['y1'], data['x2'], data['y2'], fill=data['color'])
+        self.canvas.move(self.id, data['x0'], data['y0'])
+        self.dx = 0
+        self.dxl = data['dxl']
+        self.dxr = data['dxr']
+        self.canvas.bind_all(data['left_arrow'], self.__move_left)
+        self.canvas.bind_all(data['right_arrow'], self.__move_right)
 
+    # noinspection PyUnusedLocal
+    def __move_left(self, evt):
+        self.dx = self.dxl
 
-def start():
-    tk = Tk()
-    tk.title("Ping-pong")
-    tk.resizable(0, 0)
-    tk.wm_attributes("-topmost", 1)
-    canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
-    canvas.pack()
-    tk.update()
-    ball = Ball(canvas, 'red')
-    while True:
-        tk.update_idletasks()
-        tk.update()
-        sleep(0.01)
+    # noinspection PyUnusedLocal
+    def __move_right(self, evt):
+        self.dx = self.dxr
+
+    def draw(self):
+        coords = self.canvas.coords(self.id)
+        self.dx = 0 if coords[0] <= 0 or coords[2] >= self.canvas.winfo_width() else self.dx
+        self.canvas.move(self.id, self.dx, 0)
+        self.dx = 0
 
 
 if __name__ == '__main__':
-    start()
+    Game().start()
